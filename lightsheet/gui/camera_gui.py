d944ffdf862dfc5f0673c3fbac5a5cd7a5e84300
Diego <baxua@hotmail.es>
2020-05-31 14:50:23 +0200

    Hide progress until acquisition

--- lightsheet/gui/camera_gui.py
+++ lightsheet/gui/camera_gui.py
@@ -34,7 +34,8 @@ class ViewingWidget(QWidget):
         self.setLayout(QVBoxLayout())
 
         self.image_viewer = pyqtgraph.ImageView()
-        self.roi = pyqtgraph.ROI(pos=(50, 50), size=(10, 10))
+        self.roi = self.image_viewer.getRoiPlot()
+        # self.roi = pyqtgraph.ROI(pos=(50, 50), size=(10, 10))
         self.image_viewer.ui.roiBtn.hide()
         self.image_viewer.ui.menuBtn.hide()
 
@@ -54,12 +55,17 @@ class ViewingWidget(QWidget):
         self.set_roi_button = QPushButton("set ROI")
 
         self.layout().addWidget(self.image_viewer)
+        self.layout().addWidget(self.roi)
         self.layout().addWidget(self.wid_display_settings)
         self.layout().addWidget(self.wid_camera_properties)
         self.layout().addWidget(self.lbl_camera_info)
         self.layout().addWidget(self.set_roi_button)
         self.layout().addWidget(self.chunk_progress)
         self.layout().addWidget(self.stack_progress)
+
+        self.chunk_progress.hide()
+        self.stack_progress.hide()
+
         self.is_first_image = True
         self.refresh_display = True
 
@@ -100,6 +106,8 @@ class ViewingWidget(QWidget):
 
         sstatus = self.state.get_save_status()
         if sstatus is not None:
+            self.chunk_progress.show()
+            self.stack_progress.show()
             num_chunks = int(sstatus.target_params.n_t / sstatus.target_params.chunk_size)
             self.chunk_progress.setMaximum(num_chunks)
             self.chunk_progress.setValue(sstatus.i_chunk)
