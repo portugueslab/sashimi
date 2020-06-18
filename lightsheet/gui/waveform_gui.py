257951cf89f34d3f24bc7b83eb09fef254563758
Diego <baxuaa@gmail.com>
2020-06-18 16:53:51 +0200

    camera pulse in wave plot with LinearRegionItem()

--- lightsheet/gui/waveform_gui.py
+++ lightsheet/gui/waveform_gui.py
@@ -5,15 +5,15 @@ import numpy as np
 
 
 class WaveformWidget(QWidget):
-    def __init__(self, waveform_queue, pulses_queue, timer):
+    def __init__(self, waveform_queue, pulses_queue, timer, state, sample_rate=40000):
         super().__init__()
+        self.sample_rate = sample_rate
+        self.state = state
         self.waveform_queue = waveform_queue
         self.pulses_queue = pulses_queue
         self.plot_widget = pg.PlotWidget()
         self.plot_curve = pg.PlotCurveItem()
-        self.plot_bars = pg.BarGraphItem()
         self.plot_widget.addItem(self.plot_curve)
-        self.plot_widget.addItem(self.plot_bars)
         self.setLayout(QVBoxLayout())
         self.layout().addWidget(self.plot_widget)
         timer.timeout.connect(self.update)
@@ -22,8 +22,15 @@ class WaveformWidget(QWidget):
         try:
             current_waveform = self.waveform_queue.get(timeout=0.001)
             current_pulses = self.pulses_queue.get(timeout=0.001)
-            current_pulses = np.nonzero(current_pulses)
+            pulse_timing = np.nonzero(current_pulses)[0]
+            pulse_duration = self.sample_rate / 1000 * self.state.camera_settings.exposure
             self.plot_curve.setData(np.arange(len(current_waveform)), current_waveform)
-            self.plot_bars.setData(x=current_pulses, height=1, width=1, brush='b')
+            for region in range(len(self.linear_region)):
+                self.plot_widget.removeItem(self.linear_region[region])
+            self.linear_region = [None] * len(pulse_timing)
+            for pulse in pulse_timing:
+                self.linear_region[pulse_timing] = pg.LinearRegionItem(values=(pulse, pulse + pulse_duration))
+                self.plot_widget.addItem(self.linear_region[pulse_timing])
+                self.plot_widget.removeItem()
         except Empty:
             pass
