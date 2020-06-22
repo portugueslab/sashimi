cb7cc03b4c5129acc555c964a759aafd5afbec00
Diego <baxuaa@gmail.com>
2020-06-22 11:13:18 +0200

    Lighter test

--- tests/tests.py
+++ tests/tests.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 def test_noise_subtraction():
-    t = np.random.randint(low=0, high=500, size=150, dtype=np.uint16)
-    r = np.random.randint(low=0, high=500, size=t.shape, dtype=np.uint16)
+    t = np.array([10000, 5, 200], dtype=np.uint16)
+    r = np.array([265, 20, 200], dtype=np.uint16)
     output = cast_unsigned_subtraction(t, r)
     overflows = np.where(t.astype(np.int32) - r.astype(np.int32) < 0)
     zeros = np.zeros_like(output[overflows])
