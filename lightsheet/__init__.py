ea5c281da9d7dd5673a2ffc3ae3f8edb248aed62
diego <baxuaa@gmail.com>
2020-07-15 13:37:26 +0200

    Added option for running from commandline

--- lightsheet/__init__.py
+++ lightsheet/__init__.py
@@ -1,15 +1,5 @@
-from PyQt5.QtWidgets import QApplication
-import qdarkstyle
-from lightsheet.state import State
-from lightsheet.gui.main_gui import MainWindow
-from PyQt5.QtGui import QIcon
+from lightsheet.main import main
+
 
 if __name__ == "__main__":
-    app = QApplication([])
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-    app.setApplicationName("Lightsheet control")
-    app.setWindowIcon(QIcon(r"icons/main_icon.png"))
-    st = State(sample_rate=40000)
-    main_window = MainWindow(st)
-    main_window.show()
-    app.exec()
+    main(40000)
