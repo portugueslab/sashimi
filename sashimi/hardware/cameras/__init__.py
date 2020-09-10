fb40d0a166249ed9142be1aca6d6382265e01580
diego <baxuaa@gmail.com>
2020-09-10 14:48:41 -0400

    Merged BasicCamera with AbstractCameraInterface

--- sashimi/hardware/cameras/__init__.py
+++ sashimi/hardware/cameras/__init__.py
@@ -1,16 +1,18 @@
 from sashimi.config import read_config
 import time
 import numpy as np
-from sashimi.hardware.cameras.interface import AbstractCameraInterface
 
 conf = read_config()
 
 
-class BasicCamera(AbstractCameraInterface):
+class BasicCamera:
     def __init__(self):
         super().__init__()
         self.encoding = "utf-8"
         self.camera_id = conf["camera"]["id"]
+        self.image_height = 2048
+        self.image_width = 2048
+        self.frame_shape: tuple = (1024, 1024)
 
     def apply_parameters(self, parameters):
         """
@@ -72,6 +74,61 @@ class BasicCamera(AbstractCameraInterface):
         Close down the connection to the camera.
         """
         self.stop_acquistion()
+
+    @property
+    def exposure_time(self):
+        return None
+
+    @exposure_time.setter
+    def exposure_time(self, parameters):
+        pass
+
+    @property
+    def binning(self):
+        return None
+
+    @binning.setter
+    def binning(self, parameters):
+        pass
+
+    @property
+    def subarray(self):
+        return None
+
+    @subarray.setter
+    def subarray(self, parameters):
+        pass
+
+    @property
+    def internal_frame_rate(self):
+        return None
+
+    @property
+    def trigger_mode(self):
+        return None
+
+    @trigger_mode.setter
+    def trigger_mode(self, parameters):
+        pass
+
+    @property
+    def camera_mode(self):
+        return None
+
+    @camera_mode.setter
+    def camera_mode(self, parameters):
+        pass
+
+
+class AbstractCameraConfigurator:
+    def __init__(self):
+        pass
+
+    def __enter__(self):
+        return AbstractCameraInterface()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 
 class MockCamera(BasicCamera):
