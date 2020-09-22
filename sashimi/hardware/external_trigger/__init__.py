a5cf133be7aefe2f845c83a8e0cd665863aae6ad
diego <baxuaa@gmail.com>
2020-09-21 21:44:28 -0400

    Abstract classes to interface

--- sashimi/hardware/external_trigger/__init__.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-
-class AbstractComm:
-    def __init__(self, address):
-        self.address = address
-
-    def trigger_and_receive_duration(self, config) -> Optional[float]:
-        return None
