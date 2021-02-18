b1926f96af456ea4e63fa16bf54aaf36beee19fe
Luigi Petrucco <luigi.petrucco@gmail.com>
2021-02-18 19:59:42 +0100

    Full exp test running now

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,6 +1,8 @@
 from sashimi.gui.main_gui import MainWindow
-from sashimi.state import State
+from sashimi.state import State, TriggerSettings
 import qdarkstyle
+from PyQt5.QtCore import Qt
+from split_dataset import SplitDataset
 
 
 class MockEvt:
@@ -8,20 +10,28 @@ class MockEvt:
         pass
 
 
-def test_main(qtbot):
+def test_main(qtbot, temp_path):
+
     st = State()
     style = qdarkstyle.load_stylesheet_pyqt5()
     main_window = MainWindow(st, style)
     # icon_dir = (Path(__file__).parents[0]).resolve() / "icons/main_icon.png"
     main_window.show()
-    qtbot.wait(1000)
-    # b = main_window.wid_status.findChild("Calibration")
-    for i in [1, 2, 3, 1, 0]:
-        main_window.wid_status.setCurrentIndex(i)
-        # qtbot.mouseClick(b,
-        #                  Qt.LeftButton,
-        #                  delay=1)
-        # exp.end_protocol(save=False)
-        qtbot.wait(1000)
+    qtbot.wait(300)
+    # for i in [1, 2, 3, 1, 0]:
+    main_window.wid_status.setCurrentIndex(3)
+
+    st.save_settings.save_dir = str(temp_path)
+    main_window.wid_save_options.set_locationbutton()
+    st.send_scansave_settings()
+    # Wait to send and receive parameters:
+    qtbot.wait(10000)
+    qtbot.mouseClick(main_window.toolbar.experiment_toggle_btn, Qt.LeftButton, delay=1)
+    # wait end of the experiment:
+    qtbot.wait(TriggerSettings().experiment_duration + 5000)
+
+    # try opening the result:
+    SplitDataset(temp_path / "original")
+
     main_window.closeEvent(MockEvt())
     qtbot.wait(1000)
