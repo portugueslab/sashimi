95d08d86d8a5d5c90eae3af8c62673f6529d7b55
Luigi Petrucco <luigi.petrucco@gmail.com>
2021-02-18 20:13:34 +0100

    comments added

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -11,22 +11,27 @@ class MockEvt:
 
 
 def test_main(qtbot, temp_path):
-
     st = State()
     style = qdarkstyle.load_stylesheet_pyqt5()
     main_window = MainWindow(st, style)
-    # icon_dir = (Path(__file__).parents[0]).resolve() / "icons/main_icon.png"
     main_window.show()
     qtbot.wait(300)
-    # for i in [1, 2, 3, 1, 0]:
+
+    # go to calibration and volumetric mode:
+    main_window.wid_status.setCurrentIndex(1)
+    qtbot.wait(300)
     main_window.wid_status.setCurrentIndex(3)
 
+    # Manually update new directory (to avoid nasty pop up window for filesystem):
     st.save_settings.save_dir = str(temp_path)
     main_window.wid_save_options.set_locationbutton()
     st.send_scansave_settings()
+
     # Wait to send and receive parameters:
     qtbot.wait(10000)
+
     qtbot.mouseClick(main_window.toolbar.experiment_toggle_btn, Qt.LeftButton, delay=1)
+
     # wait end of the experiment:
     qtbot.wait(TriggerSettings().experiment_duration + 5000)
 
