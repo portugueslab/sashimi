f3476f4024e3fe4a8529df8f4c23fca8bce58566
fedem-p <federico.puppo@tum.de>
2022-03-16 14:56:46 +0100

    Enforcing abstract class, methods and properties

--- sashimi/hardware/cameras/interface.py
+++ sashimi/hardware/cameras/interface.py
@@ -1,5 +1,5 @@
 from enum import Enum
-
+from abc import ABC, abstractmethod
 
 class CameraException(Exception):
     pass
@@ -14,11 +14,12 @@ class TriggerMode(Enum):
     EXTERNAL_TRIGGER = 2
 
 
-class AbstractCamera:
+class AbstractCamera(ABC):
     def __init__(self, camera_id, max_sensor_resolution=None):
         self.camera_id = camera_id
         self.max_sensor_resolution = max_sensor_resolution
-
+    
+    @abstractmethod
     def get_frames(self):
         """
         Returns a list of arrays, each of which corresponds to an available frame. If no frames where found returns an
@@ -26,18 +27,21 @@ class AbstractCamera:
         """
         pass
 
+    @abstractmethod
     def start_acquisition(self):
         """
         Allocate as many frames as will fit in 2GB of memory and start data acquisition.
         """
         pass
 
+    @abstractmethod
     def stop_acquisition(self):
         """
         Stop data acquisition and release the memory allocated for frames.
         """
         pass
 
+    @abstractmethod
     def shutdown(self):
         """
         Close down the connection to the camera.
@@ -45,38 +49,47 @@ class AbstractCamera:
         self.stop_acquisition()
 
     @property
+    @abstractmethod
     def exposure_time(self):
         return None
-
+    
     @exposure_time.setter
+    @abstractmethod
     def exposure_time(self, exp_val):
         pass
-
+    
     @property
+    @abstractmethod
     def binning(self):
         return None
 
     @binning.setter
+    @abstractmethod
     def binning(self, exp_val):
         pass
-
+    
     @property
+    @abstractmethod
     def roi(self):
         return None
 
     @roi.setter
+    @abstractmethod
     def roi(self, exp_val: tuple):
         pass
-
+    
     @property
+    @abstractmethod
     def trigger_mode(self):
         return None
 
     @trigger_mode.setter
+    @abstractmethod
     def trigger_mode(self, exp_val):
         pass
 
     @property
+    @abstractmethod
     def frame_rate(self):
         return None
 
--- sashimi/hardware/light_source/interface.py
+++ sashimi/hardware/light_source/interface.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from abc import ABC, abstractmethod
 
 
 class LaserState(Enum):
@@ -14,34 +15,40 @@ class LaserWarning(Warning):
     pass
 
 
-class AbstractLightSource:
+class AbstractLightSource(ABC):
     def __init__(self, port):
         self.port = port
         self._status = LaserState.OFF
         self.intensity_units = None
 
-    def start(self):
+    def start(self):  #TODO: remove?
         pass
-
+    
+    @abstractmethod
     def set_power(self, current):
         """Sets power of laser based on self.intensity and self.intensity_units"""
         pass
 
+    @abstractmethod
     def close(self):
         pass
 
     @property
+    @abstractmethod
     def intensity(self):
         return None
 
     @intensity.setter
+    @abstractmethod
     def intensity(self, exp_val):
         pass
 
     @property
+    @abstractmethod
     def status(self):
         return self._status
 
     @status.setter
+    @abstractmethod
     def status(self, exp_val):
         pass
